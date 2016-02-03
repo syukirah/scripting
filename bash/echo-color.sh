c3e98e048264f532f152bde63181d9fc00642619
Syukur Bin Md Kassim <syukirah@yahoo.co.uk>
2016-02-03 17:26:32 +0800

    Add folder and script to print color

---
+++ bash/echo-color.sh
@@ -0,0 +1,11 @@
+# Author: Syukur Bin Md Kassim
+# Email: syukur.kassim@tmrnd.com.my
+# Description: print to stdout with different color
+
+RED="\x1B[31m"
+GREEN="\x1B[32m"
+MAGENTA="\x1B[01;95m"
+LIGHT_RED="\x1B[01;91m"
+BLUE="\x1B[01;94m"
+GRAY="\x1B[01;36m"
+END_COLOR="\x1B[0m"
---
+++ bash/usage-echo-color.sh
@@ -0,0 +1,12 @@
+# Author: Syukur Bin Md Kassim
+# Email: syukur.kassim@tmrnd.com.my
+# Description: print to stdout with different color
+
+. echo-color.sh
+
+echo -e ${RED}red color $END_COLOR
+echo -e ${GREEN}green color $END_COLOR
+echo -e ${MAGENTA}magenta color $END_COLOR
+echo -e ${LIGHT_RED}light red color $END_COLOR
+echo -e ${BLUE}blue color $END_COLOR
+echo -e ${GRAY}gray color $END_COLOR
