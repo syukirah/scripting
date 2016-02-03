7d1e05c169722f3543ef5e71e7f15dcc0736788a
Syukur Bin Md Kassim <syukirah@yahoo.co.uk>
2016-02-03 17:38:52 +0800

    Improve print color

--- bash/echo-color.sh
+++ bash/echo-color.sh
@@ -2,10 +2,10 @@
 # Email: syukur.kassim@tmrnd.com.my
 # Description: print to stdout with different color
 
-RED="\x1B[31m"
-GREEN="\x1B[32m"
-MAGENTA="\x1B[01;95m"
-LIGHT_RED="\x1B[01;91m"
-BLUE="\x1B[01;94m"
-GRAY="\x1B[01;36m"
-END_COLOR="\x1B[0m"
+ECHO_RED="echo -e \x1B[31m"
+ECHO_GREEN="echo -e \x1B[32m"
+ECHO_MAGENTA="echo -e \x1B[01;95m"
+ECHO_LIGHT_RED="echo -e \x1B[01;91m"
+ECHO_BLUE="echo -e \x1B[01;94m"
+ECHO_GRAY="echo -e \x1B[01;36m"
+ECHO_END_COLOR="\x1B[0m"
